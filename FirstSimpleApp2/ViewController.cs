4be2e7ed1ce3b90fb26f681411c0cedccc6ec95f
lessia2020 <nurislamova2000@gmail.com>
2023-01-29 10:46:10 +0100

    second part

---
+++ FirstSimpleApp2/ViewController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace FirstSimpleApp2
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+        partial void PushButton(NSButton sender)
+        {
+			SetText("Button Pushed.");
+        }
+        partial void ResetButton(NSButton sender)
+        {
+			SetText("");
+			
+        }
+
+		private void SetText(string text)
+		{
+			InfoLabel.StringValue = text;
+		}
+
+    }
+}
---
+++ FirstSimpleApp2/ViewController.designer.cs
@@ -0,0 +1,32 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace FirstSimpleApp2
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		AppKit.NSTextField InfoLabel { get; set; }
+
+		[Action ("PushButton:")]
+		partial void PushButton (AppKit.NSButton sender);
+
+		[Action ("ResetButton:")]
+		partial void ResetButton (AppKit.NSButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (InfoLabel != null) {
+				InfoLabel.Dispose ();
+				InfoLabel = null;
+			}
+		}
+	}
+}
